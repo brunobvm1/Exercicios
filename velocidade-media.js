112340912d7652cae12a1d06458df5b7ba78f2f8
Brunobvm1 <brunobvm1@hotmail.com>
2022-02-18 17:18:27 -0300

    finalizada a equação, e apresentada a velociade média do usuário.

--- velocidade-media.js
+++ velocidade-media.js
@@ -1,5 +1,5 @@
-let distPercorrida, tempo;
+let distPercorrida = 80, tempo = 1800; //30 minutos
 
-let velocidade = distPercorrida / tempo
+let velocidade = distPercorrida / tempo;
 
-console.log(velocidade * 3, 6);
+console.log(`Sua velocidade média é de: ${velocidade * 3.6}km/h`);
