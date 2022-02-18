d5220741dec3b7b80aa7aa7bccaf315c22741348
Brunobvm1 <brunobvm1@hotmail.com>
2022-02-18 16:56:56 -0300

    Atualizada a formatação do numero

--- Juros-compostos.js
+++ Juros-compostos.js
@@ -1,4 +1,4 @@
-let capital = 1.000, juros = (12.5 / 100), peridoDeTempo = 5;
+let capital = 1000, juros = (12.5 / 100), peridoDeTempo = 5;
 
 let montante = capital * ((1 + juros) ** peridoDeTempo);
 
