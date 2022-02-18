299c3a3072df02c31a3beb4aa2ef0b13f65bfc9c
Brunobvm1 <brunobvm1@hotmail.com>
2022-02-18 16:09:15 -0300

    atualizadas as equações.

--- Juros-compostos.js
+++ Juros-compostos.js
@@ -1,3 +1,5 @@
-const capital = 1000, juros = 0.125, peridoDeTempo = 5
+const capital = 1000, juros = 0.125, peridoDeTempo = 5;
 
-const montante = capital * (1 + juros) * peridoDeTempo
+const montante = capital * (1 + juros) * peridoDeTempo;
+
+console.log(montante);
