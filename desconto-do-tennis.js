9e52dfdea2b87d548be6f55673c6f911f5604927
Brunobvm1 <brunobvm1@hotmail.com>
2022-02-18 15:29:02 -0300

    finalizada a equação, atualizada a variavel para let e apresentada o calculo de desconto.

--- desconto-do-tennis.js
+++ desconto-do-tennis.js
@@ -1,2 +1,6 @@
-const precoTennis = 129.99
-const saldoDinheiro = 80.00
+const precoTennis = 129.99;
+const saldoDinheiro = 80.00;
+const desconto = ((precoTennis - saldoDinheiro) * 100) / precoTennis;
+
+
+console.log(desconto);
