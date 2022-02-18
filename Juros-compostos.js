f4702260339ef9710948ff9d6f937c225371a75c
Brunobvm1 <brunobvm1@hotmail.com>
2022-02-18 16:16:19 -0300

    Atualizada a equação e finalizada a calculadora de juros

--- Juros-compostos.js
+++ Juros-compostos.js
@@ -1,5 +1,5 @@
-let capital = 1000, juros = 0.125, peridoDeTempo = 5;
+let capital = 1.000, juros = (12.5 / 100), peridoDeTempo = 5;
 
-let montante = capital * (1 + juros) * peridoDeTempo;
+let montante = capital * ((1 + juros) * peridoDeTempo);
 
 console.log(montante);
