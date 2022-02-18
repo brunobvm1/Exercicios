9a64f35a80dd910ae4f354c9108e86322af6146e
Brunobvm1 <brunobvm1@hotmail.com>
2022-02-18 16:54:07 -0300

    Adicionada e atualizada a equação de distancia entre pontos, adicionada potenciação

---
+++ distancia-entre-dois-pontos.js
@@ -0,0 +1,6 @@
+let pontoX_Horiz1 = 1, pontoX_Horiz2 = 1;
+let pontoY_Vert1 = 1, pontoY_Vert2 = 4;
+
+let distanciaEntrePontos = Math.sqrt(((pontoX_Horiz2 - pontoX_Horiz1) ** 2) + ((pontoY_Vert2 - pontoY_Vert1) ** 2));
+
+console.log(distanciaEntrePontos);
