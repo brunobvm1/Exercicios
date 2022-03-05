30d18a5c080e94a42190a33835c09fcbfdc5e999
Brunobvm1 <brunobvm1@hotmail.com>
2022-03-05 19:03:43 -0300

    atualizado o codigo

--- Imc.js
+++ Imc.js
@@ -1,4 +1,8 @@
 let peso = 81, altura = 1.75;
 
-let imc = peso / (altura * altura);
+let imc = peso / altura ** 2);
 console.log(imc);
+
+//let imc = peso / Math.pow(altura, 2);
+//console.log(imc);
+
