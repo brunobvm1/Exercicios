50772d9d13e7e0dd7ae53794addc1e1fb09157cc
Brunobvm1 <brunobvm1@hotmail.com>
2022-03-12 18:39:18 -0300

    atualizada a equação

---
+++ area-total-do-cilindro.js
@@ -0,0 +1,5 @@
+let pi = Math.PI, raio = 2, altura = 3;
+let aBase = 2 * pi * raio * (raio + altura);//, aLateral = 2 * pi * raio * altura, aTotal;
+
+//aTotal = (2 * aBase) + aLateral;
+console.log(aBase);
--- area-total-do-circulo.js
+++
@@ -1,5 +0,0 @@
-let pi = 3.14, raio = 5, altura = 200;
-let aBase = 2 * pi * (raio ** 2), aLateral = 2 * pi * raio * altura, aTotal;
-
-aTotal = (2 * aBase) + aLateral;
-console.log(aTotal);
