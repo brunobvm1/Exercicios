158e8e252eff78817fc827cd6fdf62b1f92371b9
Brunobvm1 <brunobvm1@hotmail.com>
2022-03-12 16:14:28 -0300

    atualizada a equação

--- volume-esfera.js
+++ volume-esfera.js
@@ -1,4 +1,9 @@
-let v, pi = 3.14, r = 3;
+let v, pi = 3.1415, r = 3;
+let d = 4;
 
-v = 4 / 3 * pi * (r * 2) ** 3;
+r = d / 2;
+v = 4 / 3 * r ** 3;
+
 console.log(`O volume de uma esfera de raio ${r} é ${v.toFixed([2])} Pi.`);
+
+console.log(`O volume de uma esfera de raio ${r} é ${(v * Math.PI).toFixed([2])} Pi.`);
