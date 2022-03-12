43afc2ac8b17557fa77c9ea7fffd2213519476a3
Brunobvm1 <brunobvm1@hotmail.com>
2022-03-12 18:26:20 -0300

    atualizada a equação

--- taxas-de-juros.js
+++ taxas-de-juros.js
@@ -1,5 +1,5 @@
 let taxaDeJuros, montante = 90000, capital = 60000;
 let numeroDePeriodos = 24;
 
-taxaDeJuros = ((montante / capital) ** (1 / numeroDePeriodos)) - 1;
-console.log(`O seu financiamento de ${capital} reais teve uma taxa de juros de ${taxaDeJuros.toFixed([3])}, pois após ${numeroDePeriodos} meses você teve que pagar ${montante} reais`)
+taxaDeJuros = ((montante / capital) ** (1 / numeroDePeriodos) - 1);
+console.log(`O seu financiamento de ${capital} reais teve uma taxa de juros de ${taxaDeJuros * 100}%, pois após ${numeroDePeriodos} meses você teve que pagar ${montante} reais`)
