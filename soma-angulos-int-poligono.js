2eb3b6523db9295011b36a2d7801aac509124db9
Brunobvm1 <brunobvm1@hotmail.com>
2022-03-12 16:01:21 -0300

    atualizada a equação

--- soma-angulos-int-poligono.js
+++ soma-angulos-int-poligono.js
@@ -1,5 +1,9 @@
-let soma = 0, nLados = 6
+let soma = 0, nLados = 6;
 
-soma = (nLados - 2) * 180
+soma = (nLados - 2) * 180;
 
-console.log(`O valor da soma de todos os angulos é: ${soma}°`)
+let angInterno = soma / nLados;
+
+console.log(`O valor da soma de todos os angulos é: ${soma}°`);
+
+console.log(`Caso os ângulos sejam iguais, cada um deles vale ${angInterno}`);
